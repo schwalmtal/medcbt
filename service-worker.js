4e476c5006b12c4c68f005db1eca1e708db1153d
schwalmtal <rzzz62rmh5@privaterelay.appleid.com>
2026-07-30 16:57:47 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C='medbank-v3-source-slides';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','app.css','app.js','courses.json']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.url.includes('/slides/'))return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
+const C='medbank-v4-jpg-slides';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','app.css','app.js','courses.json'])))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.url.includes('/slides/')){e.respondWith(fetch(e.request));return;}e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
