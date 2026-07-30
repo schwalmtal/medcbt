5444703d1ff11d0f68a70cc648f7ad869cf306cb
schwalmtal <rzzz62rmh5@privaterelay.appleid.com>
2026-07-30 16:17:16 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C='medbank-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','app.css','app.js','courses.json']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
